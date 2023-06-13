88f2118d258262e74306a5d9fa5d05800f456286
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-13 12:35:25 +0200

    modified httpRequests class

--- src/test/java/Day2/WaysToCreateRequestBody.java
+++ src/test/java/Day2/WaysToCreateRequestBody.java
@@ -1,5 +1,6 @@
 package Day2;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.testng.annotations.Test;
@@ -22,20 +23,22 @@ public class WaysToCreateRequestBody {
     @Test
     void postUsingHashMap()
     {
-        HashMap hm = new HashMap();
+        try
+        {
+            HashMap hm = new HashMap();
 
-        hm.put("name", "John");
-        hm.put("location","Angola");
-        hm.put("phone", "0447234793");
-        String coursesArr[] = {"Java","Python"};
-        hm.put("courses",coursesArr);
+            hm.put("name", "John");
+            hm.put("location","Angola");
+            hm.put("phone", "0447234793");
+            String coursesArr[] = {"Java","Python"};
+            hm.put("courses",coursesArr);
 
-        given()
+            given()
                     .contentType("application/json")
                     .body(hm)
-                .when()
+                    .when()
                     .post("https://reqres.in/api/users")
-                .then()
+                    .then()
                     .body("name", equalTo("John"))
                     .body("location", equalTo("Angola"))
                     .body("phone", equalTo("0447234793"))
@@ -43,51 +46,61 @@ public class WaysToCreateRequestBody {
                     .body("courses[1]",equalTo("Python"))
                     .statusCode(201)
                     .log().all();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //Request body using JSON Object
     @Test
     void postUsingJsonObject()
     {
-        JSONObject data = new JSONObject();
+        try
+        {
+            JSONObject data = new JSONObject();
 
-        data.put("name", "Powel");
-        data.put("location", "USA");
-        data.put("phone", "234567");
-        String courseArray[] = {"JavaScript","Ruby"};
-        data.put("course", courseArray);
+            data.put("name", "Powel");
+            data.put("location", "USA");
+            data.put("phone", "234567");
+            String courseArray[] = {"JavaScript","Ruby"};
+            data.put("course", courseArray);
 
-        given()
+            given()
                     .contentType("application/json")
                     .body(data.toString())
-                .when()
+                    .when()
                     .post("https://reqres.in/api/users")
-                .then()
+                    .then()
                     .body("name", equalTo("Powel"))
                     .body("location", equalTo("USA"))
                     .body("phone", equalTo("234567"))
                     .statusCode(201)
                     .log().all();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     //Request body using POJO
     @Test
     void postUsingPOJO()
     {
-        POJO_postRequest data = new POJO_postRequest();
+        try
+        {
+            POJO_postRequest data = new POJO_postRequest();
 
-        data.setName("Pravin");
-        data.setLocation("India");
-        data.setPhone("83893211");
-        String courses[] = {"Javascript","Python"};
-        data.setCourses(courses);
+            data.setName("Pravin");
+            data.setLocation("India");
+            data.setPhone("83893211");
+            String courses[] = {"Javascript","Python"};
+            data.setCourses(courses);
 
-        given()
+            given()
                     .contentType("application/json")
                     .body(data)
-                .when()
+                    .when()
                     .post("https://reqres.in/api/users")
-                .then()
+                    .then()
                     .body("name", equalTo("Pravin"))
                     .body("location", equalTo("India"))
                     .body("phone", equalTo("83893211"))
@@ -95,6 +108,9 @@ public class WaysToCreateRequestBody {
                     .body("courses[1]", equalTo("Python"))
                     .statusCode(201)
                     .log().all();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //Request body using external Json file
@@ -105,24 +121,31 @@ public class WaysToCreateRequestBody {
         file reader
         JSON tokener
         JSON Object*/
+        try
+        {
 
-        File myFile = new File(".//body_JSON");
-        FileReader myFileReader = new FileReader(myFile);
-        JSONTokener jsonTokener = new JSONTokener(myFileReader);
-        JSONObject myData = new JSONObject(jsonTokener);
+            File myFile = new File(".//body_JSON");
+            FileReader myFileReader = new FileReader(myFile);
+            JSONTokener jsonTokener = new JSONTokener(myFileReader);
+            JSONObject myData = new JSONObject(jsonTokener);
 
-        given()
+            given()
                     .contentType("application/json")
                     .body(myData.toString())
-                .when()
+                    .when()
                     .post("https://reqres.in/api/users")
-                .then()
+                    .then()
                     .body("name", equalTo("Marenskie"))
                     .body("location",equalTo("Germany"))
                     .body("phone",equalTo("668758"))
-                        .body("courses[0]",equalTo("C"))
+                    .body("courses[0]",equalTo("C"))
                     .body("courses[1]", equalTo("C++"))
                     .statusCode(201)
-                .log().all();
+                    .log().all();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
